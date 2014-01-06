0ce8fe440262c753ffbf6d4261460293e240b02a
Nicholas <mrmonotone95@gmail.com>
2014-01-05 22:32:00 -0800

    more housekeeping

--- Assets/Scripts/TileMapData.cs
+++ Assets/Scripts/TileMapData.cs
@@ -16,13 +16,13 @@ using System.Linq;
 
 public class TileMapData : MonoBehaviour {
 	private GameManager gm;
-	private int _Size_X;
-	private int _Size_Y;
+	private int _SizeX;
+	private int _SizeZ;
 	private Tile[,] _MapData;
 	private List<Room> _Rooms;
 	private int _MyMaxFails = 10;
 	private int _MyMaxRooms = 25;
-	private Tile floor, wall, stone;
+	private Tile floor, wall, stone, unknown;
 
 	public int MaxFails {
 		get {
@@ -66,16 +66,17 @@ public class TileMapData : MonoBehaviour {
 		floor = new Tile(1,Tile.Kind.Floor,true,"Floor",0);
 		wall = new Tile(2,Tile.Kind.Wall,false,"Wall",1);
 		stone = new Tile(3,Tile.Kind.Stone,false,"Stone",2);
+		unknown = new Tile(0,Tile.Kind.Unknown,false,"Unknown",99);
 	}
 
-	public void CreateTileMapData(int size_X, int size_Y) {
-		this._Size_X = size_X;
-		this._Size_Y = size_Y;
+	public void CreateTileMapData(int sizeX, int sizeZ) {
+		this._SizeX = sizeX;
+		this._SizeZ = sizeZ;
 
-		_MapData = new Tile[size_X,size_Y];
+		_MapData = new Tile[sizeX,sizeZ];
 
-		for(int x=0;x<size_X;x++) {
-			for(int y=0;y<size_Y;y++) {
+		for(int x=0;x<sizeX;x++) {
+			for(int y=0;y<sizeZ;y++) {
 				_MapData[x,y] = stone;
 			}
 		}
@@ -88,8 +89,8 @@ public class TileMapData : MonoBehaviour {
 			int rsy = Random.Range(4,10);
 			
 			r = new Room();
-			r.left = Random.Range(0, _Size_X - rsx);
-			r.top = Random.Range(0, _Size_Y - rsy);
+			r.left = Random.Range(0, _SizeX - rsx);
+			r.top = Random.Range(0, _SizeZ - rsy);
 			r.width = rsx;
 			r.height = rsy;
 			
@@ -112,6 +113,9 @@ public class TileMapData : MonoBehaviour {
 				MakeCorridor(_Rooms[i], _Rooms[(i + j) % _Rooms.Count ]);
 			}
 		}
+		foreach(Room rr in _Rooms) {
+			MakeDoors(rr);
+		}
 
 		MakeWalls();
 	}
@@ -127,12 +131,12 @@ public class TileMapData : MonoBehaviour {
 
 	void MakeRoom(Room r) {	
 		for(int x=0; x < r.width; x++) {
-			for(int y=0; y < r.height; y++){
-				if(x==0 || x == r.width-1 || y==0 || y == r.height-1 || x<0 || y< 0) {
-					_MapData[r.left+x,r.top+y] = wall;
+			for(int z=0; z < r.height; z++){
+				if(x==0 || x == r.width-1 || z==0 || z == r.height-1 || x<0 || z< 0) {
+					_MapData[r.left+x,r.top+z] = wall;
 				}
 				else {
-					_MapData[r.left+x,r.top+y] = floor;
+					_MapData[r.left+x,r.top+z] = floor;
 				}
 			}
 		}	
@@ -140,46 +144,60 @@ public class TileMapData : MonoBehaviour {
 	
 	void MakeCorridor(Room r1, Room r2) {
 		int x = r1.centerX;
-		int y = r1.centerZ;
+		int z = r1.centerZ;
 		
 		while( x != r2.centerX ) {
-			_MapData[x,y] = floor;
+			_MapData[x,z] = floor;
 			x += x < r2.centerX ? 1 : -1;
 		}
-		while( y != r2.centerZ ) {
-			_MapData[x,y] = floor;
-			y += y < r2.centerZ ? 1 : -1;
+		while( z != r2.centerZ ) {
+			_MapData[x,z] = floor;
+			z += z < r2.centerZ ? 1 : -1;
 		}
 		r1.isConnected = true;
 		r2.isConnected = true;	
 	}
 	
 	void MakeWalls() {
-		for(int x=0; x< _Size_X;x++) {
-			for(int y=0; y< _Size_Y;y++) {
-				if(_MapData[x,y]==stone && HasAdjacentFloor(x,y)) {
-					_MapData[x,y]= wall;
+		for(int x=0; x< _SizeX;x++) {
+			for(int z=0; z< _SizeZ;z++) {
+				if(_MapData[x,z]==stone && HasAdjacentFloor(x,z)) {
+					_MapData[x,z]= wall;
+				}
+			}
+		}
+	}
+
+	void MakeDoors(Room r){
+/* Check floor tiles outside of room
+ * using HasAdjacentFloor
+ * Place door where there are 4 Adjacent floor tile
+ */
+		for(int x=0; x < r.width; x++) {
+			for(int z=0; z < r.height; z++){
+				if(HasAdjacentFloor(x,z)){
+					_MapData[x,z] = unknown;
 				}
 			}
 		}
 	}
 	
-	bool HasAdjacentFloor(int x, int y) {
-		if( x > 0 && _MapData[x-1,y] == floor )
+	bool HasAdjacentFloor(int x, int z) {
+		if( x > 0 && _MapData[x-1,z] == floor )
 			return true;
-		if( x < _Size_X-1 && _MapData[x+1,y] == floor )
+		if( x < _SizeX-1 && _MapData[x+1,z] == floor )
 			return true;
-		if( y > 0 && _MapData[x,y-1] == floor )
+		if( z > 0 && _MapData[x,z-1] == floor )
 			return true;
-		if( y < _Size_Y-1 && _MapData[x,y+1] == floor )
+		if( z < _SizeZ-1 && _MapData[x,z+1] == floor )
 			return true;
-		if( x > 0 && y > 0 && _MapData[x-1,y-1] == floor )
+		if( x > 0 && z > 0 && _MapData[x-1,z-1] == floor )
 			return true;
-		if( x < _Size_X - 1 && y > 0 && _MapData[x+1,y-1] == floor )
+		if( x < _SizeX - 1 && z > 0 && _MapData[x+1,z-1] == floor )
 			return true;
-		if( x > 0 && y < _Size_Y-1 && _MapData[x-1,y+1] == floor )
+		if( x > 0 && z < _SizeZ-1 && _MapData[x-1,z+1] == floor )
 			return true;
-		if( x < _Size_X-1 && y < _Size_Y-1 && _MapData[x+1,y+1] == floor )
+		if( x < _SizeX-1 && z < _SizeZ-1 && _MapData[x+1,z+1] == floor )
 			return true;
 
 		return false;
